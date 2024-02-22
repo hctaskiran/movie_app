60a26a369e0dd9806a73f60e5ff0975c1a718d84
Haktan Can Taşkıran <haktancantaskiran@gmail.com>
2024-02-22 21:03:47 +0300

    Initial Commit

---
+++ movie_app/movie_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_appApp.swift
+//  movie_app
+//
+//  Created by Haktan Can Taşkıran on 22.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct movie_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
