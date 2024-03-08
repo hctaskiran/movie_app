0d272d8d93583259298b44986dfe2de01383596f
Haktan Can Taşkıran <haktancantaskiran@gmail.com>
2024-03-08 16:59:10 +0300

    added detail page

---
+++ movie_app/MovieDetailView.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  movie_app
+//
+//  Created by Haktan Can Taşkıran on 8.03.2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct MovieDetailView: View {
+    
+    let movie: Movie
+    var body: some View {
+        Text("aaaaaa")
+    }
+}
+
+struct MoviewDetailView_Preview: PreviewProvider {
+    static var previews: some View {
+        MovieDetailView(movie: .mock)
+    }
+}
